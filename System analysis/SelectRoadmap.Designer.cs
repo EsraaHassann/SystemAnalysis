ef942b2677c061ebeab98eb15afbea7d01cfa8a5
EsraaHassann <147067443+EsraaHassann@users.noreply.github.com>
2024-05-06 02:44:27 +0300

    new edits

--- System analysis/SelectRoadmap.Designer.cs
+++ System analysis/SelectRoadmap.Designer.cs
@@ -28,18 +28,267 @@
         /// </summary>
         private void InitializeComponent()
         {
+            label1 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            label2 = new Label();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            button9 = new Button();
+            button10 = new Button();
+            button11 = new Button();
+            label6 = new Label();
+            button12 = new Button();
+            button13 = new Button();
             SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Sitka Display", 16.1999989F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(509, 191);
+            label1.Name = "label1";
+            label1.Size = new Size(200, 39);
+            label1.TabIndex = 0;
+            label1.Text = "Pick a Language";
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Sitka Display", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button1.Location = new Point(205, 134);
+            button1.Name = "button1";
+            button1.Size = new Size(88, 47);
+            button1.TabIndex = 1;
+            button1.Text = "C++";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Sitka Display", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button2.Location = new Point(205, 216);
+            button2.Name = "button2";
+            button2.Size = new Size(88, 47);
+            button2.TabIndex = 2;
+            button2.Text = "C#";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Sitka Display", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button3.Location = new Point(909, 134);
+            button3.Name = "button3";
+            button3.Size = new Size(88, 47);
+            button3.TabIndex = 3;
+            button3.Text = "Java";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Font = new Font("Sitka Display", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button4.Location = new Point(909, 216);
+            button4.Name = "button4";
+            button4.Size = new Size(88, 47);
+            button4.TabIndex = 4;
+            button4.Text = "Python";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Sitka Display", 16.1999989F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(515, 415);
+            label2.Name = "label2";
+            label2.Size = new Size(194, 39);
+            label2.TabIndex = 5;
+            label2.Text = "Data Structures";
+            // 
+            // button5
+            // 
+            button5.Font = new Font("Sitka Display", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button5.Location = new Point(205, 345);
+            button5.Name = "button5";
+            button5.Size = new Size(118, 57);
+            button5.TabIndex = 6;
+            button5.Text = "Linked List";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.Font = new Font("Sitka Display", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button6.Location = new Point(205, 458);
+            button6.Name = "button6";
+            button6.Size = new Size(118, 57);
+            button6.TabIndex = 7;
+            button6.Text = "Array";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            button7.Font = new Font("Sitka Display", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button7.Location = new Point(895, 345);
+            button7.Name = "button7";
+            button7.Size = new Size(118, 57);
+            button7.TabIndex = 8;
+            button7.Text = "Tree";
+            button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            button8.Font = new Font("Sitka Display", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button8.Location = new Point(895, 458);
+            button8.Name = "button8";
+            button8.Size = new Size(118, 57);
+            button8.TabIndex = 9;
+            button8.Text = "Hash Table";
+            button8.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Sitka Display", 25.1999989F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(469, 24);
+            label3.Name = "label3";
+            label3.Size = new Size(307, 61);
+            label3.TabIndex = 10;
+            label3.Text = "Select Roadmap";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Sitka Display", 16.1999989F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.Location = new Point(484, 597);
+            label4.Name = "label4";
+            label4.Size = new Size(251, 39);
+            label4.TabIndex = 11;
+            label4.Text = "Asymptotic Notation";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Sitka Display", 16.1999989F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.Location = new Point(383, 770);
+            label5.Name = "label5";
+            label5.Size = new Size(94, 39);
+            label5.TabIndex = 12;
+            label5.Text = "Caches";
+            // 
+            // button9
+            // 
+            button9.Font = new Font("Sitka Display", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button9.Location = new Point(226, 695);
+            button9.Name = "button9";
+            button9.Size = new Size(82, 54);
+            button9.TabIndex = 13;
+            button9.Text = "LFU";
+            button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            button10.Font = new Font("Sitka Display", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button10.Location = new Point(226, 765);
+            button10.Name = "button10";
+            button10.Size = new Size(82, 54);
+            button10.TabIndex = 14;
+            button10.Text = "LRU";
+            button10.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            button11.Font = new Font("Sitka Display", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button11.Location = new Point(226, 835);
+            button11.Name = "button11";
+            button11.Size = new Size(82, 54);
+            button11.TabIndex = 15;
+            button11.Text = "MFU";
+            button11.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Sitka Display", 16.1999989F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label6.Location = new Point(715, 770);
+            label6.Name = "label6";
+            label6.Size = new Size(130, 39);
+            label6.TabIndex = 16;
+            label6.Text = "Searching";
+            // 
+            // button12
+            // 
+            button12.Font = new Font("Sitka Display", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button12.Location = new Point(895, 695);
+            button12.Name = "button12";
+            button12.Size = new Size(136, 57);
+            button12.TabIndex = 17;
+            button12.Text = "Binary Search";
+            button12.UseVisualStyleBackColor = true;
+            // 
+            // button13
+            // 
+            button13.Font = new Font("Sitka Display", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button13.Location = new Point(895, 832);
+            button13.Name = "button13";
+            button13.Size = new Size(136, 57);
+            button13.TabIndex = 18;
+            button13.Text = "Linear Search";
+            button13.UseVisualStyleBackColor = true;
             // 
             // SelectRoadmap
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(894, 627);
+            ClientSize = new Size(1226, 1032);
+            Controls.Add(button13);
+            Controls.Add(button12);
+            Controls.Add(label6);
+            Controls.Add(button11);
+            Controls.Add(button10);
+            Controls.Add(button9);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(label2);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label1);
             Name = "SelectRoadmap";
             Text = "SelectRoadmap";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Label label2;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Button button9;
+        private Button button10;
+        private Button button11;
+        private Label label6;
+        private Button button12;
+        private Button button13;
     }
 }
